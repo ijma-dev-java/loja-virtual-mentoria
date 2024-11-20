9b6d458cc6814e7017072f5986deccb4ad8bb02d
Suporte <Suporte@DESKTOP-U61DUC6>
2024-11-20 20:44:43 -0300

    20-11-2024 - Criando Banco e Classes com Spring Data JPA

---
+++ src/main/java/br/com/loja/virtual/mentoria/model/StatusRastreio.java
@@ -0,0 +1,105 @@
+package br.com.loja.virtual.mentoria.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "status_rastreio")
+@SequenceGenerator(name = "status_rastreio_seq", sequenceName = "status_rastreio_seq", allocationSize = 1, initialValue = 1)
+public class StatusRastreio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "status_rastreio_seq")
+	private Long id;
+
+	private String centroDistribuicao;
+
+	private String cidade;
+
+	private String estado;
+
+	private String status;
+
+	@ManyToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk"))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCentroDistribuicao() {
+		return centroDistribuicao;
+	}
+
+	public void setCentroDistribuicao(String centroDistribuicao) {
+		this.centroDistribuicao = centroDistribuicao;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
+
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StatusRastreio other = (StatusRastreio) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}

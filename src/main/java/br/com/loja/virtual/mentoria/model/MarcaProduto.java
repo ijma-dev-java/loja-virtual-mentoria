f123fcafe4ec5bb065ecfb0f188471cc80b1635d
Suporte <Suporte@DESKTOP-U61DUC6>
2024-11-20 02:29:04 -0300

    20-11-2024 - Criação e configuração do banco de dados com o application.properties:

---
+++ src/main/java/br/com/loja/virtual/mentoria/model/MarcaProduto.java
@@ -0,0 +1,41 @@
+package br.com.loja.virtual.mentoria.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "marca_produto_seq", sequenceName = "marca_produto_seq", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "marca_produto_seq")
+	private Long id;
+
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}

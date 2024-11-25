f7117fac28e85ee34550a9c6bec24f5bea8ad283
Suporte <Suporte@DESKTOP-U61DUC6>
2024-11-25 00:49:39 -0300

    Cadastro de Pessoa Jurídica e Usuário com Junit com endereço de cobrança e de entrega

--- src/test/java/br/com/loja/virtual/mentoria/PessoaUsuarioTest.java
+++ src/test/java/br/com/loja/virtual/mentoria/PessoaUsuarioTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 import br.com.loja.virtual.mentoria.controller.PessoaUsuarioController;
+import br.com.loja.virtual.mentoria.enums.TipoEndereco;
+import br.com.loja.virtual.mentoria.model.Endereco;
 import br.com.loja.virtual.mentoria.model.PessoaJuridica;
 import junit.framework.TestCase;
 
@@ -33,6 +35,42 @@ public class PessoaUsuarioTest extends TestCase {
 		pessoaJuridica.setInscricaoMunicipal("45655456456465");
 		pessoaJuridica.setNomeFantasia("ijma services tecnologia");
 		pessoaJuridica.setRazaoSocial("italo jose de melo araujo me");
+
+		// Instância de enderecoCobranca
+		Endereco enderecoCobranca = new Endereco();
+
+		// Seta os atributos de enderecoCobranca
+		enderecoCobranca.setLogradouro("sdfsdfsdfsdfsdf");
+		enderecoCobranca.setCep("55554545");
+		enderecoCobranca.setNumero("3223");
+		enderecoCobranca.setComplemento("fsfsfsfsd");
+		enderecoCobranca.setBairro("fdsfsfsfsd");
+		enderecoCobranca.setCidade("adasdasdadasd");
+		enderecoCobranca.setUf("PE");
+		enderecoCobranca.setTipoEndereco(TipoEndereco.COBRANCA);
+		enderecoCobranca.setPessoa(pessoaJuridica);
+		enderecoCobranca.setEmpresa(pessoaJuridica);
+
+		// Instância de enderecoEntrega
+		Endereco enderecoEntrega = new Endereco();
+
+		// Seta os atributos de enderecoEntrega
+		enderecoEntrega.setLogradouro("sdfsdfsdfsdfsdf");
+		enderecoEntrega.setCep("55554545");
+		enderecoEntrega.setNumero("3223");
+		enderecoEntrega.setComplemento("fsfsfsfsd");
+		enderecoEntrega.setBairro("fdsfsfsfsd");
+		enderecoEntrega.setCidade("adasdasdadasd");
+		enderecoEntrega.setUf("PE");
+		enderecoEntrega.setTipoEndereco(TipoEndereco.ENTREGA);
+		enderecoEntrega.setPessoa(pessoaJuridica);
+		enderecoEntrega.setEmpresa(pessoaJuridica);
+
+		// Adicionando o enderecoCobranca na lista da pessoaJuridica
+		pessoaJuridica.getEnderecos().add(enderecoCobranca);
+
+		// Adicionando o enderecoEntrega na lista da pessoaJuridica
+		pessoaJuridica.getEnderecos().add(enderecoEntrega);
 
 		// Chamando o pessoaController
 		pessoaController.salvarPessoaJuridica(pessoaJuridica);
